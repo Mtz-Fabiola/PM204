582939c96dd3952e7015d569ea38472b3c1a66f4
Mtz-Fabiola <fm0560240@gmail.com>
2026-06-18 23:18:49 -0600

    ActivityKeyboard

--- Practica5/miapp2/screens/ActivityKeyboardScreen.js
+++ Practica5/miapp2/screens/ActivityKeyboardScreen.js
@@ -1,57 +1,178 @@
-/* Zona 1: Importaciones, componentes y archivos */
+import React, {useState} from 'react';
+import {StatusBar} from 'expo-status-bar';
 
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, TextInput, KeyboardAvoidingView, Platform} from 'react-native';
+import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform, TouchableOpacity, 
+  ActivityIndicator, Alert, View} from 'react-native';
 
 
-/* Zona 2: Main - Renderizado de componentes */
-export default function KeyboardScreen() {
+export default function ActivityIndicator_KeyboardAvoidingView(){
 
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [correo, setCorreo] = useState('');
-  const [telefono, setTelefono] = useState('');
-  const [ciudad, setCiudad] = useState('');
-  const [contrasena, setContrasena] = useState('');
+  const[nombre, setNombre] = useState('');
+  const[correo, setCorreo] = useState('');
+  const[contrasena, setContrasena] = useState('');
 
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior='height'>
-      <TextInput style={styles.input} placeholder="Nombre" value={nombre} onChangeText={setNombre}/>
+  const[cargando, setCargando] = useState(false);
+  const[mensaje, setMensaje] = useState('');
 
-      <TextInput style={styles.input} placeholder="Apellido" value={apellido} onChangeText={setApellido}/>
+  const iniciarSesion = () =>{
 
-      <TextInput style={styles.input} placeholder="Correo" keyboardType="email-address" value={correo} onChangeText={setCorreo}/>
+    if(nombre ===''|| correo === '' || contrasena===''){
+      Alert.alert('Campos vacíos', 'Complete todos los campos');
+      return;
+    }
 
-      <TextInput style={styles.input} placeholder="Teléfono" keyboardType="phone-pad" value={telefono} onChangeText={setTelefono}/>
+    setMensaje('');
+    setCargando(true);
 
-      <TextInput style={styles.input} placeholder="Ciudad" value={ciudad} onChangeText={setCiudad}/>
+    setTimeout(() => {
 
-      <TextInput style={styles.input} placeholder="Contraseña" secureTextEntry={true} value={contrasena} onChangeText={setContrasena}/>
+      setCargando(false);
 
-      <StatusBar style="auto" />
+      setMensaje('Datos válidos. Bienvenido(a)'+nombre);
+
+    }, 3000);
+
+  };
+
+
+  return(
+
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding':'height'}>
+    
+      <Text style={styles.titulo}>
+        ActivityIndicator y KeyboardAvoidingView
+
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Correo"
+        keyboardType="email-address"
+        value={correo}
+        onChangeText={setCorreo}
+
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+        value={contrasena}
+        onChangeText={setContrasena}      
+      />
+
+      {
+        cargando ?(
+
+          <View style={styles.spinnerContainer}>
+
+            <ActivityIndicator
+              size="large"
+              color="blue"
+              animating={cargando}
+            />
+
+            <Text>
+              Validando información...
+            </Text>
+
+          </View>
+        ) : (
+
+          <TouchableOpacity
+            style={styles.boton}
+            onPress={iniciarSesion}
+          >
+
+          <Text style={styles.textoBoton}>
+            Iniciar Sesión
+          </Text>
+
+          </TouchableOpacity>
+
+        )
+      }
+
+      <Text style={styles.mensaje}>
+        {mensaje}
+      </Text>
+
+      <StatusBar style="auto"/>
+    
     </KeyboardAvoidingView>
+
+
+
   );
+
+
 }
 
 
-/* Zona 3: Estilos y posicionamiento */
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom: 50,
+ const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    backgroundColor:'#ffffff',
+    justifyContent:'center',
+    alignItems:'center',
+    padding: 20
   },
 
-  input: {
-    width: '80%',
+  titulo:{
+    fontSize:24,
+    fontWeight:'bold',
+    marginBottom:20,
+    textAlign:'center'
+  },
+
+  input:{
+    width:'90%',
     height: 50,
     borderWidth: 1,
     borderColor: '#000',
-    marginVertical: 8,
-    paddingHorizontal: 10,
-    borderRadius: 8,
+    borderRadius:8, 
+    paddingHorizontal:10,
+    marginVertical:5
   },
-});
+
+  boton:{
+    backgroundColor: '#2196F3',
+    width: '90%',
+    padding: 15,
+    borderRadius:8,
+    marginTop:20
+  },
+
+  textoBoton:{
+    color:'#ffffff',
+    textAlign: 'center',
+    fontWeight: 'bold'
+
+  },
+
+  spinnerContainer:{
+    alignItems: 'center',
+    marginTop:20
+  },
+
+  mensaje:{
+
+    marginTop:20,
+    fontSize:18,
+    fontWeight:'bold',
+    color: 'green',
+    textAlign: 'center'
+
+  }
+
+ });
